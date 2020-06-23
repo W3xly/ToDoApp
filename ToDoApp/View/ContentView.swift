ed37f56ce9fc6f2d5cd902094b8c83156ec8605f
Jan Podmolík <wexly.ue@gmail.com>
2020-06-23 21:09:57 +0200

    fetching todos from coredata, implemented delete & edit button

--- ToDoApp/View/ContentView.swift
+++ ToDoApp/View/ContentView.swift
@@ -14,33 +14,61 @@ struct ContentView: View {
     
     // enable to access internal storage
     @Environment(\.managedObjectContext) var managedObjectContext
+    // sortDescriptor -> sorted by name -> true = alphabetical order
+    @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
     
     @State private var showingAddTodoView: Bool = false
     
     //MARK: - Body
     var body: some View {
         NavigationView {
-            List(0..<5) { item in
-                Text("Hello!")
+            List {
+                ForEach(self.todos, id: \.self) { todo in
+                    HStack {
+                        Text(todo.name ?? "Unknown")
+                        Spacer()
+                        Text(todo.priority ?? "Unknown")
+                    }
+                } //END: ForEach
+                .onDelete(perform: deleteTodo)
             } //END: List
-            .navigationBarTitle("Todo", displayMode: .inline)
-            .navigationBarItems(trailing:
-                Button(action: {
-                    self.showingAddTodoView.toggle()
-                }, label: {
-                    Image(systemName: "plus")
-                }) //END: Button
-                    .sheet(isPresented: $showingAddTodoView, content: {
-                        // passing manage object here
-                        AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
-                    })
+                .navigationBarTitle("Todo", displayMode: .inline)
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing:
+                    Button(action: {
+                        self.showingAddTodoView.toggle()
+                    }, label: {
+                        Image(systemName: "plus")
+                    }) //END: Button
+                        .sheet(isPresented: $showingAddTodoView, content: {
+                            // passing manage object here
+                            AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+                        })
             )
         } //END: NavigationView
     } //END: body
+    
+    //MARK: - Helpers
+    
+    // specialy made for .onDelete function that take func of this type
+    private func deleteTodo(at offsets: IndexSet) {
+        for index in offsets {
+            let todo = todos[index]
+            managedObjectContext.delete(todo)
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print("DEBUG: error: \(error.localizedDescription)")
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let context = (UIApplication.shared.delegate as! AppDelegate)
+            .persistentContainer.viewContext
+        return ContentView().environment(\.managedObjectContext, context)
     }
 }
