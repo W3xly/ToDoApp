35212b3406f154e0c78ebec25a9100b7fb3ff050
Jan Podmolík <wexly.ue@gmail.com>
2020-06-23 16:28:59 +0200

    Initial Commit

---
+++ ToDoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//
+//  Created by Johana Šlechtová on 23/06/2020.
+//  Copyright © 2020 Jan Podmolík. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
